b6fe3c5e75f1c7bd3b98887a159fe75180bd9042
Andrey Semenov <sema0517@yandex.ru>
2022-11-09 20:29:29 +0300

    2nd module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = Number()
+  for (k in salaries){
+    if (typeof salaries[k] == 'number'&&isFinite(salaries[k])){
+    sum += salaries[k];
+    }
+   }
+  return sum
 }
